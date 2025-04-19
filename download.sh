546cb5e13f471d2e7394d63a73e441297656293b
RI MAXIM <maxim.ree@googlemail.com>
2025-04-19 17:06:25 +0200

    Create download.sh

---
+++ download.sh
@@ -0,0 +1,41 @@
+wget https://data.ace.uq.edu.au/public/gtdb/data/releases/release226/226.0/bac120_r226.tree.gz
+wget https://data.ace.uq.edu.au/public/gtdb/data/releases/release226/226.0/bac120_metadata_r226.tsv.gz
+gunzip *gz
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+ 1Help               2Save               3Mark               4Replac             5Copy               6Move               7Search             8Delete             9PullDn            10Quit
