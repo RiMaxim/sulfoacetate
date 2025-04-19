16d585d2bed5bc63c663df1131809ee3a2499b1a
RI MAXIM <maxim.ree@googlemail.com>
2025-04-19 18:59:13 +0200

    Update download.sh

--- download.sh
+++ download.sh
@@ -1,5 +1,7 @@
 wget https://data.ace.uq.edu.au/public/gtdb/data/releases/release226/226.0/bac120_r226.tree.gz
 wget https://data.ace.uq.edu.au/public/gtdb/data/releases/release226/226.0/bac120_metadata_r226.tsv.gz
+wget https://ftp.ncbi.nih.gov/biosample/biosample_set.xml.gz
+
 gunzip *gz
 
 
